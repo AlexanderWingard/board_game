b2ed44bc0c2b6cadcb5b55b1816efe58112643ad
Alexander Wingård <alexander.wingard@gmail.com>
2022-10-21 02:52:57 +0200

    Add lift effect

--- Input.gd
+++ Input.gd
@@ -3,7 +3,9 @@ extends Node
 onready var camera = $"../CameraRig/Camera"
 onready var camera_rig = $"../CameraRig"
 var fake_mouse_pos = Vector2.ZERO
+var initial_position = Vector3.ZERO
 var sensitivity = 0.005
+
 
 func _physics_process(delta):
 	var ground_plane = Plane(Vector3.UP, 0)
@@ -11,6 +13,7 @@ func _physics_process(delta):
 	var to = from + camera.project_ray_normal(fake_mouse_pos) * 1000
 	var cursor_pos = ground_plane.intersects_ray(from, to)
 	if pressed and cursor_pos:
+		cursor_pos.y = clamp(initial_position.distance_to(cursor_pos) / 4.0, 0, 0.4)
 		$"../Pawn".global_translation = cursor_pos
 		$"../ColorRect".rect_position = fake_mouse_pos
 		$"../Highlighter".global_translation = cursor_pos
@@ -18,6 +21,7 @@ func _physics_process(delta):
 var pressed = false
 var rotating = false
 var old_mouse_offset = Vector2.ZERO
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		if pressed:
@@ -34,6 +38,7 @@ func _input(event):
 				Input.warp_mouse_position(fake_mouse_pos + old_mouse_offset)
 			pressed = false
 			$"../Highlighter".monitoring = false
+			$"../Pawn".global_translation.y = 0
 
 func _on_Area_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -42,7 +47,8 @@ func _on_Area_input_event(camera, event, position, normal, shape_idx):
 		if event.pressed:
 			rotating = false
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-			fake_mouse_pos = camera.unproject_position($"../Pawn".global_transform.origin)
+			initial_position = $"../Pawn".global_transform.origin
+			fake_mouse_pos = camera.unproject_position(initial_position)
 			old_mouse_offset = get_viewport().get_mouse_position() - fake_mouse_pos
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
